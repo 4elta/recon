235c81e09f44c2d6fd0da2dffe1fb34b04c8babd
delta <4elta@users.noreply.github.com>
2023-03-28 13:32:23 +0200

    improve DNS analysis

--- analyzers/dns/__init__.py
+++ analyzers/dns/__init__.py
@@ -51,25 +51,24 @@ class Analyzer:
         if ipaddress.ip_address(service['address']).is_global:
           service['public'] = True
 
-      if 'public' in self.recommendations:
-        if service['public']:
-          if not self.recommendations['public']:
-            issues.append("public DNS server")
+      if service['public']:
+        if 'public' in self.recommendations and not self.recommendations['public']:
+          issues.append("public DNS server")
 
-          if service['recursive']:
-            if service['transport_protocol'].upper() == 'UDP':
-              issues.append("supports recursive DNS: this could be abused for traffic amplification attacks")
-              # https://www.cloudflare.com/learning/dns/what-is-recursive-dns/
-
-            if 'ECS' in self.recommendations:
-              if service['ECS'] and not self.recommendations['ECS']:
-                  issues.append("supports ECS: this might decrease users' privacy and could enable targeted DNS poisoning attacks")
-                  # https://yacin.nadji.us/docs/pubs/dimva16_ecs.pdf
-              if not service['ECS'] and self.recommendations['ECS']:
-                issues.append("does not support ECS: this might hinder load balancing")
+        if service['recursive']:
+          if service['transport_protocol'].upper() == 'UDP':
+            issues.append("supports recursive DNS: this could be abused for traffic amplification attacks")
+            # https://www.cloudflare.com/learning/dns/what-is-recursive-dns/
 
       if 'DNSSEC' in self.recommendations:
         if self.recommendations['DNSSEC'] and not service['DNSSEC']:
           issues.append("does not validate DNSSEC: this could lead to DNS cache poisoning")
+
+      if service['recursive'] and 'ECS' in self.recommendations:
+        if service['ECS'] and not self.recommendations['ECS']:
+          issues.append("supports ECS: this might decrease users' privacy and could enable targeted DNS poisoning attacks")
+          # https://yacin.nadji.us/docs/pubs/dimva16_ecs.pdf
+        if not service['ECS'] and self.recommendations['ECS']:
+          issues.append("does not support ECS: this might hinder load balancing")
 
     return services
